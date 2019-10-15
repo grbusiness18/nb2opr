bbe38ac6fe0fcbcaae58f5deeb2740231df05ffd
GR <grbusiness18@gmail.com>
2019-10-15 15:19:39 +0200

    Updated __init to load automatically

--- nb2opr/__init__.py
+++ nb2opr/__init__.py
@@ -3,12 +3,8 @@ __version__ = '0.0.1'
 
 from .nb2opr import DIMagic
 
-def load_ipython_extension(ipython):
-    """
-    Any module file that define a function named `load_ipython_extension`
-    can be loaded via `%load_ext module.path` or be configured to be
-    autoloaded by IPython at startup time.
-    """
-    # You can register the class itself without instantiating it.  IPython will
-    # call the default constructor on it.
-    ipython.register_magics(DIMagic)
+try:
+    ip = get_ipython()
+    ip.register_magics(DIMagic)
+except:
+    pass
