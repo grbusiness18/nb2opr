19835619b7d55ab68b02d0e8bd7ced24c47a007f
GR <grbusiness18@gmail.com>
2019-10-31 15:27:27 +0100

    Stable version 0.0.1

--- build/lib/nb2opr/__init__.py
+++
@@ -1,7 +0,0 @@
-""" DI Notebook 2 Operator Helper """
-__version__ = '0.0.2'
-
-from .exporters import add_code_to_operator, add_connections_to_port, add_port_to_operator, \
-    create_operator, create_pipeline, get_pipelines, set_log_level
-
-from .utility_objects import PortObject, ConnectionObject, OperatorObject, PortKind, ContentType
